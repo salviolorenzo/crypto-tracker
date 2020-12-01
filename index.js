29f1c443b11b4db103a41dbb1998edf77ac38364
salviolorenzo <salviolorenzo@gmail.com>
2020-11-30 20:44:02 -0500

    changing data returned

--- index.js
+++ index.js
@@ -3,36 +3,42 @@ const fetch = require('node-fetch')
 const apiKey = '41e10b99-20d0-4200-8bac-a1782b7228d6';
 const apiUrl = 'pro-api.coinmarketcap.com/'
 
-const getListings = (tickers = []) => {
-  const prices = [];
+const getListings = async (tickers = []) => {
+  // const prices = [];
 
-  fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {qs: {
+ const data = await fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {qs: {
     'start': '1',
     'limit': '5000',
     'convert': 'USD'
   },
   headers: {
+    'Accept': 'application/json' ,'Accept-Encoding': 'deflate, gzip',
     'X-CMC_PRO_API_KEY': apiKey
   }
   })
   .then(res => res.json())
   .then(response => {
-    response.data.forEach(crypto => {
-      if(tickers.indexOf(crypto.symbol) !== -1){
-        prices.push({
+    return response.data.filter(crypto => tickers.indexOf(crypto.symbol) !== -1).map(crypto => {
+        return {
             ...crypto,
             quote: crypto.quote.USD
-        })
-      }
+        }
     })
-
-    console.log(prices)
-
   }).catch((err) => {
     console.log('API call error:', err.message);
   });
 
+  const bigDrops = [];
+   data.forEach(item => {
+    if(item.quote.percent_change_1h < -1){
+      bigDrops.push({
+        ticker: item.symbol,
+        change: item.quote.percent_change_1h
+      });
+    }
+  });
 
+  console.log(bigDrops);
 }
 
 getListings(['BTC', 'ETC', 'XRP', 'XLM', 'LTC', 'ETH']); 
