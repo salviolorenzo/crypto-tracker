6e494852022a2f587d0ba9f26c0552da764d8bb4
salviolorenzo <salviolorenzo@gmail.com>
2020-11-30 21:11:03 -0500

    adding emial

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
 const fetch = require('node-fetch')
+const nodemailer = require('nodemailer');
 
-const apiKey = '41e10b99-20d0-4200-8bac-a1782b7228d6';
+
+const apiKey = process.env.API_KEY ;
 const apiUrl = 'pro-api.coinmarketcap.com/'
 
 const getListings = async (tickers = []) => {
@@ -18,22 +21,27 @@ const getListings = async (tickers = []) => {
   })
   .then(res => res.json())
   .then(response => {
-    return response.data.filter(crypto => tickers.indexOf(crypto.symbol) !== -1).map(crypto => {
+    
+    if(response.data){
+      
+      return response.data.filter(crypto => tickers.indexOf(crypto.symbol) !== -1).map(crypto => {
         return {
-            ...crypto,
-            quote: crypto.quote.USD
+          ...crypto,
+          quote: crypto.quote.USD
         }
-    })
+      })
+    }
   }).catch((err) => {
     console.log('API call error:', err.message);
   });
 
   const bigDrops = [];
-   data.forEach(item => {
-    if(item.quote.percent_change_1h < -1){
+  data.forEach(item => {
+    if(item.quote.percent_change_1h < 0){
       bigDrops.push({
         ticker: item.symbol,
-        change: item.quote.percent_change_1h
+        change: item.quote.percent_change_1h.toFixed(3),
+        price: item.quote.price.toFixed(3)
       });
     }
   });
@@ -41,4 +49,38 @@ const getListings = async (tickers = []) => {
   console.log(bigDrops);
 }
 
-getListings(['BTC', 'ETC', 'XRP', 'XLM', 'LTC', 'ETH']); 
+  const sendEmail = () => {
+console.log(process.env)
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
+  }
+});
+
+const mailOptions = {
+  from: process.env.MAIL_USER,
+  to: process.env.MAIL_USER,
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
+  }
+
+
+
+sendEmail();
+
+  setInterval(
+   () =>  getListings(['BTC', 'ETC', 'XRP', 'XLM', 'LTC', 'ETH']), 5000
+  )
+
+// getListings(['BTC', 'ETC', 'XRP', 'XLM', 'LTC', 'ETH']); 
